769bf0e718a138bd11cd735de44dfc5983229b14
hiverkiya <hiverkiya@gmail.com>
2022-03-02 16:43:42 +0530

    Implementing Sanity API

--- Medium/medium/pages/post/[slug].tsx
+++ Medium/medium/pages/post/[slug].tsx
@@ -1,13 +1,35 @@
 import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
-import PortableText from "react-portable-text"
+import PortableText from 'react-portable-text'
 import { Post } from '../../typings'
 import { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 interface Props {
   post: Post
 }
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
 function Post({ post }: Props) {
+  const [submited, setSubmited] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+  const onSubmit:SubmitHandler<IFormInput>=async(data)=>{ fetch('/api/createComment',{
+  method:'POST',
+  body:JSON.stringify(data),
+}).then(()=>{
+  console.log(data);
+}).catch((err) =>{
+  console.log(err);
+});
+  };
   return (
     <main>
       <Header />
@@ -21,44 +43,97 @@ function Post({ post }: Props) {
         <h2 className="mb-2 text-xl font-light text-gray-500">
           {post.description}
         </h2>
-        <div className='flex items-center space-x-2'>
+        <div className="flex items-center space-x-2">
           <img
             className="h10 w-10 rounded-full"
             src={urlFor(post.author.image).url()!}
           />
           <p className="text-sm font-extralight">
-            Blog post by <span className='text-green-600'>{post.author.name}</span> - Published at{' '}
-            {new Date(post._createdAt).toLocaleString()}
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
           </p>
         </div>
         <div className="mt-10">
           <PortableText
-          className=""
-          dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
-          projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
-          content={post.body}
-          serializers={{
-            h1: (props: any) => (
-              <h1 className="my-5 text-2xl font-bold" {...props} />
-            ),
-            h2: (props: any) => (
-              <h2 className="my-5 text-xl font-bold" {...props} />
-            ),
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
 
-            li: ({ children }: any) => (
-              <ul>
-                <li className="ml-4 list-disc">{children}</li>
-              </ul>
-            ),
-            link: ({ href, children }: any) => (
-              <a href={href} className="text-blue-400 hover:underline">
-                {children}
-              </a>
-            ),
-          }}
+              li: ({ children }: any) => (
+                <ul>
+                  <li className="ml-4 list-disc">{children}</li>
+                </ul>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-400 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
           />
         </div>
       </article>
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
+      <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mb-10 flex max-w-2xl flex-col p-5">
+        <h3 className="text-sm text-yellow-500">Enjoyed the article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below</h4>
+        <hr className="mt-2 py-3" />
+        <input {...register('_id')} type="hidden" name="_id" value={post._id} />
+        <label className="mb-5 block">
+          <span className="text-gray-700">Name</span>
+          <input
+            {...register('name', { required: true })}
+            placeholder="Enter your name"
+            type="text"
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+          />
+        </label>
+        <label className="mb-5 block">
+          <span className="text-gray-700">Email</span>
+          <input
+            {...register('email', { required: true })}
+            placeholder="Put your email here"
+            type="email"
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+          />
+        </label>
+        <label className="mb-5 block">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            {...register('comment', { required: true })}
+            className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            placeholder="Love to hear a comment"
+            rows={8}
+          />
+        </label>
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">The Name Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">
+              - The Comment Field is required
+            </span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">The Email Field is required</span>
+          )}
+        </div>
+        <input
+          type="submit"
+          className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4  font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
+        />
+      </form>
+      {/* Returning errors here */}
     </main>
   )
 }
