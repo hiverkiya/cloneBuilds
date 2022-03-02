f0487a326457db97fcf16175083a323a2cbe9094
hiverkiya <hiverkiya@gmail.com>
2022-03-02 17:24:22 +0530

    Implementing comments

--- Medium/medium/pages/api/createComment.ts
+++ Medium/medium/pages/api/createComment.ts
@@ -11,9 +11,25 @@ const config = {
 }
 
 const client = sanityClient(config)
-export default function createComment(
+export default async function createComment(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const {_id,name,email,comment}=JSON.parse(req.body);
+    try {
+        await client.create({
+            _type:'comment',
+            post:{
+                _type:'reference',
+                _ref: _id
+            },
+            name,
+            email,
+            comment
+        })
+    }
+    catch(err){
+        return res.status(500).json({message:' Error submitting comment', err})
+    }
+  return res.status(200).json({message: 'Comment Submitted'})
 }
--- Medium/medium/pages/post/[slug].tsx
+++ Medium/medium/pages/post/[slug].tsx
@@ -21,15 +21,20 @@ function Post({ post }: Props) {
     handleSubmit,
     formState: { errors },
   } = useForm<IFormInput>()
-  const onSubmit:SubmitHandler<IFormInput>=async(data)=>{ fetch('/api/createComment',{
-  method:'POST',
-  body:JSON.stringify(data),
-}).then(()=>{
-  console.log(data);
-}).catch((err) =>{
-  console.log(err);
-});
-  };
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data)
+        setSubmited(true)
+      })
+      .catch((err) => {
+        console.log(err)
+        setSubmited(false)
+      })
+  }
   return (
     <main>
       <Header />
@@ -83,68 +88,107 @@ function Post({ post }: Props) {
         </div>
       </article>
       <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
-      <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mb-10 flex max-w-2xl flex-col p-5">
-        <h3 className="text-sm text-yellow-500">Enjoyed the article?</h3>
-        <h4 className="text-3xl font-bold">Leave a comment below</h4>
-        <hr className="mt-2 py-3" />
-        <input {...register('_id')} type="hidden" name="_id" value={post._id} />
-        <label className="mb-5 block">
-          <span className="text-gray-700">Name</span>
-          <input
-            {...register('name', { required: true })}
-            placeholder="Enter your name"
-            type="text"
-            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
-          />
-        </label>
-        <label className="mb-5 block">
-          <span className="text-gray-700">Email</span>
-          <input
-            {...register('email', { required: true })}
-            placeholder="Put your email here"
-            type="email"
-            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
-          />
-        </label>
-        <label className="mb-5 block">
-          <span className="text-gray-700">Comment</span>
-          <textarea
-            {...register('comment', { required: true })}
-            className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
-            placeholder="Love to hear a comment"
-            rows={8}
-          />
-        </label>
-        <div className="flex flex-col p-5">
-          {errors.name && (
-            <span className="text-red-500">The Name Field is required</span>
-          )}
-          {errors.comment && (
-            <span className="text-red-500">
-              - The Comment Field is required
-            </span>
-          )}
-          {errors.email && (
-            <span className="text-red-500">The Email Field is required</span>
-          )}
+      {submited ? (
+        <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 py-10 text-white">
+          <h3>Thank you for submitting</h3>
+          <p>Comment will appear after approval</p>
         </div>
-        <input
-          type="submit"
-          className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4  font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
-        />
-      </form>
-      {/* Returning errors here */}
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+        >
+          <h3 className="text-sm text-yellow-500">Enjoyed the article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below</h4>
+          <hr className="mt-2 py-3" />
+          <input
+            {...register('_id')}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
+          <label className="mb-5 block">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register('name', { required: true })}
+              placeholder="Enter your name"
+              type="text"
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Email</span>
+            <input
+              {...register('email', { required: true })}
+              placeholder="Put your email here"
+              type="email"
+              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            />
+          </label>
+          <label className="mb-5 block">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register('comment', { required: true })}
+              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+              placeholder="Love to hear a comment"
+              rows={8}
+            />
+          </label>
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">The Name Field is required</span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">
+                - The Comment Field is required
+              </span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">The Email Field is required</span>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4  font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
+          />
+        </form>
+      )}
+      {/*Comments */}
+      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}: </span>
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
 
 export default Post
 export const getStaticPaths = async () => {
-  const query = `
-*[_type =="post"]{
+  const query = `*[_type == "post" && slug.current == $slug][0]{
     _id,
-    slug{current}
-}`
+    _createdAt,
+    title,
+    author-> {
+    name,
+    image
+  },
+  'comments':*[
+    _type == "comment" &&
+    post._ref == ^._id &&
+    approved == true],
+  description,
+  mainImage,
+  slug,
+  body
+  }`
   const posts = await sanityClient.fetch(query)
   const paths = posts.map((post: Post) => ({
     params: {
