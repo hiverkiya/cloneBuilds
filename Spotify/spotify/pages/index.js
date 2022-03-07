1b371de6bd3b5f9b7d0ca7837b8f84731a03cb8c
hiverkiya <hiverkiya@gmail.com>
2022-03-07 11:06:14 +0530

    Setting up Vanilla JS with NextJS

--- Spotify/spotify/pages/index.js
+++ Spotify/spotify/pages/index.js
@@ -1,20 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Sidebar from "../components/Sidebar";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className>
+    <div className="bg-black h-screen overflow-hidden">
       <Head>
         <title>Spotify</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <main>
-      {/*Sidebar */}
-      {/* Center*/}
-    </main>
+      <main className="">
+        {/*Sidebar */}
+        <Sidebar />
+        {/* Center*/}
+      </main>
       <div>{/*Player*/}</div>
-
     </div>
-  )
+  );
 }
