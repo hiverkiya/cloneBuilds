1d2415b083f436561440760e49bddd971e9a369c
hiverkiya <hiverkiya@gmail.com>
2022-03-07 10:32:47 +0530

    Setting up Vanilla JS with NextJS

---
+++ Spotify/spotify/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className>
+      <Head>
+        <title>Spotify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <main>
+      {/*Sidebar */}
+      {/* Center*/}
+    </main>
+      <div>{/*Player*/}</div>
+
+    </div>
+  )
+}
