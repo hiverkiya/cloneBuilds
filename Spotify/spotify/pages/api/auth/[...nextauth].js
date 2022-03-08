a1b0eddeefeafa9c2651f5e776076143621082a9
hiverkiya <hiverkiya@gmail.com>
2022-03-08 14:38:10 +0530

    Adding sessionPersistence

--- Spotify/spotify/pages/api/auth/[...nextauth].js
+++ Spotify/spotify/pages/api/auth/[...nextauth].js
@@ -57,5 +57,11 @@ export default NextAuth({
       console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
       return await refreshAccessToken(token);
     },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken=token.refreshToken;
+      session.user.username=token.username;
+      return session;
+    },
   },
 });
