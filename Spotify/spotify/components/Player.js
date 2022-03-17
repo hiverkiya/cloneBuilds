07a34faf8637c4d2542d8006679182bc7029149f
hiverkiya <hiverkiya@gmail.com>
2022-03-17 22:15:21 +0530

    Adding Player

---
+++ Spotify/spotify/components/Player.js
@@ -0,0 +1,81 @@
+import {   HeartIcon,VolumeDownIcon } from "@heroicons/react/outline";
+import { useSession } from "next-auth/react";
+import {SwitchHorizontalIcon,RewindIcon,FastForwardIcon,PauseIcon,PlayIcon,ReplyIcon,VolumeUpIcon} from "@heroicons/react/solid"
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
+import useSpotify from "../hooks/useSpotify";
+
+function Player() {
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+  const songInfo = useSongInfo();
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        console.log("Now playing: ", data.body?.item);
+        setCurrentTrackId(data.body?.item?.id);
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+          setIsPlaying(data.body?.is_playing);
+        });
+      });
+    }
+  };
+  const handlePlayPause = () =>{
+      spotifyApi.getMyCurrentPlaybackState().then.apply((data)=>{
+          if(data.body.is_playing){
+              spotifyApi.pause();
+              setIsPlaying(false)
+          }else{
+              spotifyApi.play()
+              setIsPlaying(true)
+          }
+      })
+  }
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      //fetching the song info
+      fetchCurrentSong();
+      setVolume(50);
+    }
+  }, [currentTrackIdState, spotifyApi, session]);
+  return (
+    <div className="h-24 text-white bg-gradient-to-b from-black to-gray-900 grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+      {/*Left */}
+      <div className=" flex items-center space-x-4">
+        <img
+          className="hidden md:inline h-10 w-10"
+          src={songInfo?.album.images?.[0].url}
+        />
+        <div>
+            <h3>{songInfo?.name}</h3>
+            <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-evenly">
+    <SwitchHorizontalIcon className="button"/>
+    <RewindIcon 
+    className="button"
+    />
+    {
+        isPlaying?(
+            <PauseIcon onClick={handlePlayPause} className="button w-10 h-10"/>
+        ):(
+            <PlayIcon onClick={handlePlayPause} className="button w-10 h10"/>
+        )
+    }
+    <FastForwardIcon className="button"/>
+    <ReplyIcon className="button"/>
+
+      </div>
+      {/*Right hand side */}
+    </div>
+  );
+}
+
+export default Player;
